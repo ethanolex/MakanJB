e3b7d7096e0b9a8c1b5304a6883153aa0c4f3220
ArthurBarthurCarthur <arthursohjr@gmail.com>
2025-04-11 22:32:58 +0800

    Create App.js

---
+++ App(React Native)/App files/App.js
@@ -0,0 +1,38 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import RestaurantDetails from './RestaurantDetails';
+import RestaurantGallery from './RestaurantGallery';
+import Homepage from './Homepage'
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Details" 
+          component={RestaurantDetails} 
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="Gallery" 
+          component={RestaurantGallery} 
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="Homepage" 
+          component={Homepage} 
+          options={{ headerShown: false }}
+        />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+
+
+export default App;
