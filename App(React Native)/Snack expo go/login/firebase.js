29c049da616e32be3315aaa771bad6cc37083c57
Ethan <145786326+ethanolex@users.noreply.github.com>
2025-05-02 20:06:20 +0800

    Create firebase.js

---
+++ App(React Native)/Snack expo go/login/firebase.js
@@ -0,0 +1,24 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// Initialize services
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+export { auth, firestore, firebase };
